b26756c438edcf1d7fb9bd4b92f5f78206bf20f8
Aariaanit <arianittershnjaku@gmail.com>
2023-05-01 23:12:35 +0200

    Day4

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -12,13 +12,7 @@
     <title>Admin Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{asset('admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css')}}">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="{{asset('admin/css/sb-admin-2.min.css')}}" rel="stylesheet">
+   @include('includes.style')
 
 </head>
 
@@ -697,13 +691,7 @@
             <!-- End of Main Content -->
 
             <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
-                    </div>
-                </div>
-            </footer>
+           @include('includes.footer');
             <!-- End of Footer -->
 
         </div>
@@ -738,7 +726,7 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-   
+    @include('includes.script');
 </body>
 
 </html>
