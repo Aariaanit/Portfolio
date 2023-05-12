6055baa9c10a415360f097f120164c0e5798a26a
Aariaanit <arianittershnjaku@gmail.com>
2023-05-13 01:03:23 +0200

    Day13

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <!-- Scripts -->
     {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 </head>
-<body>
+<body class="bg-gradient-primary">
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
