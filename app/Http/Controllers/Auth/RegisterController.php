d036af9020ec33484fa591155df75afcd507d9f1
Aariaanit <arianittershnjaku@gmail.com>
2023-05-16 23:57:00 +0200

    Day16

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
@@ -29,8 +30,19 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
+    public function authenticated(){
 
+        if(Auth::user()->role_as == '1'){//1=admin
+            return redirect('/admin/dashboard')->with('status', 'Welcome to Admin Dashboard');
+        }
+        else if(Auth::user()->role_as == '0') {//0=user
+            return redirect('/login')->with('status', 'Logged In Successful');
+        }
+        else{
+            return redirect('/login');
+        }
+    }
     /**
      * Create a new controller instance.
      *
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+<link href="{{asset('admin/css/sb-admin-2.min.css')}}" rel="stylesheet">
+<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -10,7 +15,7 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}" class="user">
                         @csrf
-                            <div class="col-sm-6 mb-3 mb-sm-0">
+                            <div class="col-sm-12 mb-3 mb-sm-0">
                                 <input id="name" type="text" class="form-control form-control-user @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Name">
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -18,8 +23,9 @@
                                     </span>
                                 @enderror
                             </div>
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="email">
+                            <br>
+                            <div class="col-md-12">
+                                <input id="email" type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -27,6 +33,7 @@
                                     </span>
                                 @enderror
                             </div>
+                            <br>
                         <div class="form-group row">
                             <div class="col-sm-6 mb-3 mb-sm-0">
                                 <input id="password" type="password" class="form-control form-control-user @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Password">
@@ -38,20 +45,16 @@
                                 @enderror
                             </div>
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Repeat Password">
+                                <input id="password-confirm" type="password" class="form-control form-control-user" name="password_confirmation" required autocomplete="new-password" placeholder="Repeat Password">
                             </div>
                         </div>
                         <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-12">
                                 <button type="submit" class="btn btn-primary btn-user btn-block">
                                     {{ __('Register Account') }}
                                 </button>
                             </div>
                         </div>
-                    </form>
-                    <form class="user">
-                       
-                       
                         <hr>
                         <a href="index.html" class="btn btn-google btn-user btn-block">
                             <i class="fab fa-google fa-fw"></i> Register with Google
