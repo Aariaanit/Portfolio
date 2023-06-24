9e3f1c92c71d0da94510ed18f3e90cf882ca7e70
Aariaanit <arianittershnjaku@gmail.com>
2023-06-25 00:07:03 +0200

    Day32

--- resources/views/Includes/styles.blade.php
+++ resources/views/Includes/styles.blade.php
@@ -1,4 +1,4 @@
-<link rel="icon" type="image/png" href="{{ asset('assets/images/favicon.png') }}">
+<link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}">
    <!--favicon-img-->
    <!--main css file should not be removed -->
     <link rel="stylesheet" href="{{ asset('assets/css/index.css') }}">
